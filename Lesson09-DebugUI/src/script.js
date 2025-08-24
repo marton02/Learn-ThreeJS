8645044b90fd397d4a55abc6f1d6f54fad87e4e1
molmar <marton3molnar@gmail.com>
2025-08-24 16:04:10 +0200

    Finish Lesson 09

--- Lesson09-DebugUI/src/script.js
+++ Lesson09-DebugUI/src/script.js
@@ -1,6 +1,25 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import GUI from 'lil-gui'
+
+
+/**
+ * Debug
+ */
+const gui = new GUI({
+    width: 300,
+    title: "Nice DebugUI",
+    closeFolders: false,
+})
+// gui.close()
+gui.hide()
+window.addEventListener("keydown",(event)=>{
+    if(event.key == 'h'){
+        gui.show(gui._hidden)
+    }
+})
+const debugObject = {}
 
 /**
  * Base
@@ -14,10 +33,36 @@ const scene = new THREE.Scene()
 /**
  * Object
  */
+debugObject.color = "#a778d8"
+
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const material = new THREE.MeshBasicMaterial({ color: '#ff0000' })
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+const cubeTweaks = gui.addFolder("Awsome cube!")
+// cubeTweaks.close()
+
+// gui.add(mesh.position,"y", -3, 3, .01)
+cubeTweaks.add(mesh.position,"y").min(-3).max(3).step(0.01).name("elevation")
+cubeTweaks.add(mesh,"visible").name("isVisible")
+cubeTweaks.add(material,"wireframe").name("isWireframe")
+// gui.add(mesh.material,"wireframe").name("isWireframe")
+cubeTweaks.addColor(debugObject,"color").onChange(()=>{
+    material.color.set(debugObject.color)
+})
+
+debugObject.spin = () => {
+    gsap.to(mesh.rotation,{y: mesh.rotation.y + Math.PI * 2 })
+}
+
+cubeTweaks.add(debugObject,"spin").name("Spin it!")
+
+debugObject.subdivision = 2
+cubeTweaks.add(debugObject,"subdivision").min(1).max(20).step(1).onFinishChange(()=>{
+    mesh.geometry.dispose()
+    mesh.geometry = new THREE.BoxGeometry(1, 1, 1, debugObject.subdivision, debugObject.subdivision, debugObject.subdivision)
+})
 
 /**
  * Sizes
